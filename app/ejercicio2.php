e4d585a61f0b0a052a4b8a85a058cf91fbf74d59
javi-beltran <olivieri.jal@gmail.com>
2021-04-16 21:53:13 -0300

    subo ejercicio2

--- app/ejercicio2.php
+++ app/ejercicio2.php
@@ -1,6 +1,8 @@
 <?php
 //al ingresar un precio se muestra el precio más IVA 
 
-$precio==rand(1,1000);
-	echo $precio*1.21;
+$precio=rand(1,1000);
+	echo "precio es ".$precio ;
+	echo "<br>";
+	echo "Precio con iva 21%: ".$precio*1.21;
 ?>
