1c3495b5e41937bba239ee3bc12eb06113ce9123
javi-beltran <olivieri.jal@gmail.com>
2021-04-23 19:20:55 -0300

    ejercicio 4 correjido

--- app/ejercicio4.php
+++ app/ejercicio4.php
@@ -8,7 +8,8 @@ $edad2=rand(1,90);
 
 $edad3=rand(1,90);
 
-$variable_aux;
+//$variable_aux;
+$numMayor="nada";
 
 echo "Edad 1 " . $edad1 ." años";
 	echo "<br>";
@@ -23,6 +24,19 @@ echo "Edad 3 " . $edad3 ." años";
 // proceso
 
 	if ($edad1>$edad2 && $edad1>$edad3){
+		$numMayor=$edad1 ;
+	}else
+	{
+		if($edad2>$edad3){
+			$numMayor=$edad2;	
+		}else
+			{
+				$numMayor=$edad3;		
+			}
+	}
+	echo "<h1>La edad mayor es ->  ".$numMayor ." </h1>";		
+/*
+	if ($edad1>$edad2 && $edad1>$edad3){
 		echo "<h1>La edad mayor es edad 1 ".$edad1 ." </h1>";
 	}else
 	{
@@ -30,10 +44,10 @@ echo "Edad 3 " . $edad3 ." años";
 			echo "<h1>La edad mayor es edad 2 ".$edad2 ." </h1>";	
 		}else
 			{
-			echo "<h1>La edad mayor es edad 3 ".$edad3 ." </h1>";		
+				echo "<h1>La edad mayor es edad 3 ".$edad3 ." </h1>";		
 			}
 	}
-
+*/
 
 /* LA IDEA ES "AGRUPAR LOS IF"
 		if ($edad1 > $edad2) {
