4818c5fa668dd5d8eab02810dce11a6a39fb9df0
javi-beltran <olivieri.jal@gmail.com>
2021-05-07 21:59:17 -0300

    otr

---
+++ app/calcular2.php
@@ -0,0 +1,20 @@
+<?php
+/*funciones q se llame sumar y otra divir
+recibe numero 1 y 2 = q division funcion si es 0 para el divisor SI retorna 0 ponemos INFINITO
+otra retornar si es 0 TRUE sino es 0 FALSE
+recibe numero ingresado
+funcion calcular
+recibe numero 1 operador y numero 2
+si el operando = + usar case llamamo al suma
+si el dividir llamo al dividir
+multiplica y resta va como default en el case
+si pasa por alguna de suma o dividir llamamos a la funcion MOSTRAR RESULTADO
+FUNCION mostrar resultado
+recibe resultado como parametro
+*/
+
+
+include_once "ejercicio13.php";
+calcule(rand(-20,100),"/",rand(-20,100));
+//calcule(5,"/",0);
+?>
